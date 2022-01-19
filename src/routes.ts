56c10a30fd03db6633e19f6ac4a76074cbc88641
Raphaël Painter <raphapainter@wanadoo.fr>
2022-01-19 10:05:27 +0100

    building(feat(user inscription))

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,16 @@
-import * as express from "express"
-import * as UserController from "./controllers/userController"
-import  User  from './models/user'
+import * as express from 'express';
+import * as UserController from './controllers/userController';
+import User from './models/user';
 
-export const register = ( app: express.Application ) => {
+export const register = (app: express.Application) => {
   app.get('/', (req, res) => res.send('Hello World!'));
 
-  app.get('/user', (req, res) => {
-	res.status(200).json(UserController.listUsers())
-  })
+  app.get('/users', (req, res) => {
+    res.status(200).json(UserController.listUsers());
+  });
 
   app.post('/user', (req, res) => {
-	const newUser: User = req.body
-	res.status(200).json(UserController.addUser(newUser))
-  })
-}
+    const newUser: User = req.body;
+    res.status(200).json(UserController.addUser(newUser));
+  });
+};

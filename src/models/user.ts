a64f041efc688332a2c24becd4253e000f3d3e7b
Simon SASSI <12765875+maxerbox@users.noreply.github.com>
2022-01-19 09:24:20 +0100

    fix: user interface

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,15 +3,8 @@ import Deck from './deck';
 export default interface User {
   id: number;
   name: string; //TODO doit être unique
-  deck: Deck;
-  statut: boolean; //true = online, false = outline
+  deck?: Deck;
+  statut?: boolean; //true = online, false = outline
   score: number;
   password: string;
-}
-
-export interface User {
-  id: number;
-  username: string; //TODO doit être unique
-  password: string;
-  score: number;
 }

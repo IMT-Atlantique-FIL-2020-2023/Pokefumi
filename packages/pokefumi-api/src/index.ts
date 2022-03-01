86082cf9096f1e02daa8f4cb72b5341eb30e94f0
Simon SASSI <12765875+maxerbox@users.noreply.github.com>
2022-03-01 19:50:42 +0100

    fix: add matchmaking export pokefumi-api

--- packages/pokefumi-api/src/index.ts
+++ packages/pokefumi-api/src/index.ts
@@ -1,3 +1,4 @@
 export * as Round from './lib/generated-sources/open-api-ts-codegen/round';
 export * as User from './lib/generated-sources/open-api-ts-codegen/user';
 export * as Stats from './lib/generated-sources/open-api-ts-codegen/stats';
+export * as Matchmaking from './lib/generated-sources/open-api-ts-codegen/matchmaking';

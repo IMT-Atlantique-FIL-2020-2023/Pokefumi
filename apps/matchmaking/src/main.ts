7c6fe50f957555af4e09214bcbe594e8a9a6738c
mlhoutel <mlhoutel@gmail.com>
2022-03-01 22:09:20 +0100

    Correction des addresses.

--- apps/matchmaking/src/main.ts
+++ apps/matchmaking/src/main.ts
@@ -2,7 +2,9 @@ import { config } from 'dotenv';
 import { app } from './app/app.matchmaking';
 import { AddressInfo } from 'net';
 config();
-const server = app.listen(3334, 'localhost', () => {
+
+const port = Number(process.env.PORT || 3334);
+const server = app.listen(port, '0.0.0.0',() => {
   const { port, address } = server.address() as AddressInfo;
   console.log('Server listening on:', 'http://' + address + ':' + port);
 });

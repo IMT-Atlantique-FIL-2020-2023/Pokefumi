8da2be8dcfab670a41a46536cb2902ddc7ca73c8
Simon SASSI <12765875+maxerbox@users.noreply.github.com>
2022-03-02 22:24:06 +0100

    test add e2e skeleton test

---
+++ packages/pokefumi-e2e/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'pokefumi-e2e',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/pokefumi-e2e',
+};
---
+++ packages/pokefumi-e2e/src/lib/pokefumi-e2e.spec.ts
@@ -0,0 +1,32 @@
+import { fork, ChildProcess } from 'child_process';
+import { waitUntil } from 'async-wait-until';
+import { User, Matchmaking, Round, Stats } from '@pokefumi/pokefumi-api';
+const JWT_SECRET = 'ILIKETOTESTPOTATOES';
+const processes: ChildProcess[] = [];
+beforeAll(async () => {
+  // on fork chaque processus node pour chaque service. Bref on les lance tous en meme temps
+  for (const name of ['stats', 'user', 'round', 'matchmaking']) {
+    fork(`dist/apps/${name}/main.js`, {
+      env: {
+        JWT_SECRET,
+      },
+    });
+  }
+  await Promise.all([
+    waitUntil(async () => (await User.UserService.getAllUsers().catch(() => null)) !== null, { intervalBetweenAttempts: 500 }),
+    // waitUntil(async () => (await Matchmaking.MatchesService.getAllMatches().catch(() => null)) !== null),
+    // waitUntil(async () => (await Round.DefaultService.get().catch(() => null)) !== null),
+    // waitUntil(async () => (await Stats.RoundService.getRoundsAdayLast30Days().catch(() => null)) !== null),
+  ]);
+}, 20 * 1000);
+describe('pokefumiE2e', () => {
+  it('should work', () => {
+    expect(null).toEqual('pokefumi-e2e');
+  });
+});
+
+afterAll(() => {
+  processes.forEach(function (child) {
+    child.kill();
+  });
+});

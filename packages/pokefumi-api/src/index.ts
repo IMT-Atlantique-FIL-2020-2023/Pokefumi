1324bc73104c6e7bd3df2f777c3b7fc3ad81ac7a
Simon SASSI <12765875+maxerbox@users.noreply.github.com>
2022-03-04 23:01:50 +0100

    feat(pokefumi-api): add env variable for services base urls

--- packages/pokefumi-api/src/index.ts
+++ packages/pokefumi-api/src/index.ts
@@ -1,3 +1,13 @@
+import * as _User from './lib/generated-sources/open-api-ts-codegen/user';
+import * as _Round from './lib/generated-sources/open-api-ts-codegen/round';
+import * as _Stats from './lib/generated-sources/open-api-ts-codegen/stats';
+import * as _Matchmaking from './lib/generated-sources/open-api-ts-codegen/matchmaking';
+
+_User.OpenAPI.BASE = process.env.BASE_URL_USER || _User.OpenAPI.BASE;
+_Round.OpenAPI.BASE = process.env.BASE_URL_ROUND || _Round.OpenAPI.BASE;
+_Stats.OpenAPI.BASE = process.env.BASE_URL_STATS || _Stats.OpenAPI.BASE;
+_Matchmaking.OpenAPI.BASE = process.env.BASE_URL_MATCHMAKING || _Matchmaking.OpenAPI.BASE;
+
 export * as Round from './lib/generated-sources/open-api-ts-codegen/round';
 export * as User from './lib/generated-sources/open-api-ts-codegen/user';
 export * as Stats from './lib/generated-sources/open-api-ts-codegen/stats';
